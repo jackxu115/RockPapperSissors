1788d59ebc09e7dc5304278a017bf731c349e762
pengxu19890115@gmail.com <pengxu19890115@gmail.com>
2022-07-18 21:56:01 -0700

    update js

--- js/main.js
+++ js/main.js
@@ -1,65 +1,120 @@
-let computerSelection;
-let playerSelection;
+let computerSelection
+let playerSelection
 
-let rockImg = document.getElementById("rock");
-let paperImg = document.getElementById("paper");
-let scissorsImg = document.getElementById("scissors");
+let rockImg = document.getElementById("rock")
+let paperImg = document.getElementById("paper")
+let scissorsImg = document.getElementById("scissors")
+let restartBtn = document.getElementById("restart")
 
-let result;
-let finalResult;
-let countPlay = 0;
 
-rockImg.onclick = function () {
-    playerSelection = rockImg.getAttribute("value");
-    computerSelection = getComputerChoice();
-    result = playRound(playerSelection, computerSelection);
-    countPlay++;
+let countPlay = 5
+let playerScore = 0
+let computerScore = 0
+
+
+rockImg.onclick = () => click()
+paperImg.onclick = () => click()
+scissorsImg.onclick = () => click()
+
+function click () {
+    playerSelection = scissorsImg.getAttribute("value")
+    computerSelection = getComputerChoice()
+    playRound(playerSelection, computerSelection)
+    countPlay--
+    document.getElementById("numPlay").innerHTML = countPlay
+    game()
 }
 
-paperImg.onclick = function () {
-    playerSelection = paperImg.getAttribute("value");
-    computerSelection = getComputerChoice();
-    result = playRound(playerSelection, computerSelection);
-    countPlay++
+restartBtn.onclick = function () {
+    restart ()
 }
 
-scissorsImg.onclick = function () {
-    playerSelection = scissorsImg.getAttribute("value");
-    computerSelection = getComputerChoice();
-    result = playRound(playerSelection, computerSelection);
+function restart () {
+    countPlay = 5
+    playerScore = 0
+    computerScore = 0
+    document.getElementById("playerScore").innerHTML = playerScore
+    document.getElementById("computerScore").innerHTML = computerScore
+    document.getElementById("restart").style.display = 'none'
+    document.getElementById("finalResult").innerHTML= ''
+    document.getElementById("singleResult").innerHTML = ''
+    document.getElementById("numPlay").innerHTML = countPlay
+    rockImg.onclick = () => click()
+    paperImg.onclick = () => click()
+    scissorsImg.onclick = () => click()
+    rockImg.style.cursor = 'pointer'
+    paperImg.style.cursor = 'pointer'
+    scissorsImg.style.cursor = 'pointer'
 }
 
 function getComputerChoice () {
-    let randomNumber = Math.floor(Math.random() * 3);
+    let randomNumber = Math.floor(Math.random() * 3)
     if (randomNumber === 0) {
-        return 'rock';
+        return 'rock'
     } else if (randomNumber === 1) {
-        return 'paper';
+        return 'paper'
     } else {
-        return 'scissors';
+        return 'scissors'
     }
 }
 
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "Tie"
+        document.getElementById("singleResult").innerHTML = "Tie Game"
     } else if (playerSelection === "rock") {
         if (computerSelection === "paper") {
-            return "You Lose! Paper beats rock";
+            computerScore++
+            document.getElementById("computerScore").innerHTML = computerScore
+            document.getElementById("singleResult").innerHTML = "You Lose! Paper beats rock"
         } else {
-            return "You Win! Rock bests scissors";
+            playerScore++
+            document.getElementById("playerScore").innerHTML = playerScore
+            document.getElementById("singleResult").innerHTML = "You Win! Rock bests scissors"
         }
     } else if (playerSelection ==="paper") {
         if (computerSelection === "rock") {
-            return "You Win! Paper beats rock";
+            playerScore++
+            document.getElementById("playerScore").innerHTML = playerScore
+            document.getElementById("singleResult").innerHTML = "You Win! Paper beats rock"
         } else {
-            return "You Lose! Scissors bests paper";
+            computerScore++
+            document.getElementById("computerScore").innerHTML = computerScore
+            document.getElementById("singleResult").innerHTML = "You Lose! Scissors bests paper"
         }
     } else {
-        if (computerSelection === "scissors") {
-            return "You Lose! Rock beats scissors";
+        if (computerSelection === "rock") {
+            computerScore++
+            document.getElementById("computerScore").innerHTML = computerScore
+            document.getElementById("singleResult").innerHTML = "You Lose! Rock beats scissors"
         } else {
-            return "You Win! Scissor bests paper";
+            playerScore++
+            document.getElementById("playerScore").innerHTML = playerScore
+            document.getElementById("singleResult").innerHTML = "You Win! Scissor bests paper"
         }
     }
 }
+
+
+function finalResult() {
+    if (playerScore === computerScore) {
+        document.getElementById("finalResult").innerHTML = "It is a tie game"
+    } else if (playerScore > computerScore) {
+        document.getElementById("finalResult").innerHTML =  "You Win the game"
+    } else {
+        document.getElementById("finalResult").innerHTML = "You lost the game"
+    }
+}
+
+function game() {
+    if (countPlay === 0) {
+        finalResult()
+        document.getElementById("restart").style.display = 'block'
+        rockImg.style.cursor = 'default'
+        paperImg.style.cursor = 'default'
+        scissorsImg.style.cursor = 'default'
+        rockImg.onclick = function (e) {e.stopPropagation()}
+        paperImg.onclick = function (e) {e.stopPropagation()}
+        scissorsImg.onclick = function (e) {e.stopPropagation()}
+    }
+}
+
